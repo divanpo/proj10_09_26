cb76c951219bc7569a7744bc5c9bf6ab01486d2a
Катаев Михаил Андреевич <s25380109@unn.ru>
2026-09-10 11:42:02 +0300

    version 1 btw

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-	std::cout << "Hello world!\n";
-	return 0;
+int main()
+{
+	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
+
+	sf::CircleShape shape( 50.f );
+	sf::CircleShape shape2( 25.f );
+	shape2.setPosition({150/2, 150/2});
+	shape.setPosition({100/2, 100/2});
+
+	shape.setFillColor( sf::Color::Red );
+	shape2.setFillColor( sf::Color::White );
+
+	while ( window.isOpen() )
+	{
+		while ( const std::optional event = window.pollEvent() )
+		{
+			if ( event->is<sf::Event::Closed>() )
+				window.close();
+		}
+
+		window.clear();
+		window.draw( shape );
+		window.draw( shape2 );
+		window.display();
+	}
 }
